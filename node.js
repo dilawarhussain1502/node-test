90d1d05f8cbc721832a10bb4f480625f80f6e47a
dilawarhussain1502 <dilawarhussainhsp@gmail.com>
2026-09-13 10:26:22 -0700

    Update node.js

--- node.js
+++ node.js
@@ -1,11 +1,1 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World! Node.js App Running on AWS CodeBuild & ECS</h1>');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+console.log("docker mastey)";
